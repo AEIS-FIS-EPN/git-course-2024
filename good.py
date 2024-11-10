5fdc0d995b59e56058058159eac0b0f17f9c5db9
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-11-10 04:58:38 -0500

    chore: add functions documentation

--- good.py
+++ good.py
@@ -4,10 +4,12 @@ response1 = int(input("Ingresa un número: "))
 response2 = int(input("Ingresa otro número: "))
 
 
+# Está función sirve para sumar dos números
 def sum(num1, num2):
     return num1 + num2
 
 
+# Está función sirve para restar dos números
 def substract(num1, num2):
     return num1 - num2
 
